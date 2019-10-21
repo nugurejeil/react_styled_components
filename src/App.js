e4b6898eee1b2a52b2898f9236e6feb87653a65f
jaehyun han <>
2019-10-21 13:16:47 +0900

    delete css

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
-import React, {Component, Fragment} from 'react';
-import "./App.css"
+import React, {Component} from 'react';
+import styled from 'styled-components';
 
 
 class App extends Component {
   render(){
     return (
-      <Fragment>
-      <button className="btn btn_success">Hello!</button>
-      <button className="btn btn_danger">Hello!</button>
-      </Fragment>
+      <Container>
+        <Button>Hello!</Button>
+        <Button danger>Hello!</Button>
+      </Container>
     );
   }
 }
+const Container = styled.div`
+  height:100vh;
+  width:100%;
+  background:#bdc3c7;
+`;
+const Button = styled.button`
+  border-radius:50px;
+  padding:5px;
+  min-width: 120px;
+  color:white;
+  -webkit-appearance: none;
+  cursor: pointer;
+  background-color: ${props => (props.danger? "#e74c3c" : "#2ecc71")}
+  &:active,
+  &:focus {
+    outline: none;
+  }
+`;
 
 export default App;
